31cda1aaee89195ded58e25f1110fdff7d0afdc5
COTOPESq <127554013+COTOPESq@users.noreply.github.com>
2024-02-08 08:43:22 +0300

    2 files update

--- lab1/Header.h
+++ lab1/Header.h
@@ -1,7 +1,18 @@
 #ifndef HEADER_H 
 #define HEADER_H
 
-#include"function.cpp"
+class kvad
+{
+private:
+	double a, b, c;
 
+public:
+	kvad(double a_val, double b_val, double c_val);
+	double getA();
+	double getB();
+	double getC();
+	double calculatedisc();
+	void printfun();
 
+};
 #endif
--- lab1/function.cpp
+++ lab1/function.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include "Header.h"
+
 using namespace std;
 
-class kvad {
-private:
-	double a, b, c;
-public:
-	kvad(double a_val, double b_val, double c_val) : a(a_val), b(b_val), c(c_val) {}
+kvad::kvad(double a_val, double b_val, double c_val) : a(a_val), b(b_val), c(c_val) {}
 
-	double getA() {
-		return a;
-	}
+double kvad::getA() {
+	return a;
+}
 
-	double getB() {
-		return b;
-	}
-	double gerC() {
-		return c;
-	}
+double kvad::getB() {
+	return b;
+}
+double kvad::getC() {
+	return c;
+}
 
-	double calculatedisc() {
-		return b * b - 4 * a * c;
-	}
+double kvad::calculatedisc() {
+	return b * b - 4 * a * c;
+}
 
-	void printfun() {
-		cout << "a: " << a << " b: " << b << " c: " << c << endl;
-	}
-};
+void kvad::printfun() {
+	cout << "a: " << a << " b: " << b << " c: " << c << endl;
+}
